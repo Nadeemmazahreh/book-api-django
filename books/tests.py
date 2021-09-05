51bcae2fd36a2e08bc6abf3e05f605ad8dce903d
Nadeemmazahreh <nadeemmazahreh97@hotmail.com>
2021-09-05 18:31:57 +0300

    not finished

---
+++ books/tests.py
@@ -0,0 +1,6 @@
+from django import test
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Book
+
+# Create your tests here.
---
+++ books/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from django.urls import path, include
+from .views import BookList, BookDetail
+
+urlpatterns = [
+    path('', BookList.as_view(), name='book_list'),
+    path('<int:pk>/', BookDetail.as_view(), name='book_detail'),
+]
